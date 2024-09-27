a4d7f06bcd92115bd20085fd69119b3e486d768d
Karthik237G <gollapallikarthik03@gmail.com>
2024-09-27 23:45:13 +0530

    Update Sentiment analysis.py

--- Sentiment analysis.py
+++ Sentiment analysis.py
@@ -16,7 +16,7 @@ def sentiment_analysis(text):
 
 
 # Example usage
-text = "The NSE Nifty 50 index was down 0.4% at 24,199.9, and the S&P BSE Sensex had shed 0.38% to 79,161.1. Both indexes were down about 0.3% ahead of the decision. They extended losses to about 0.6% immediately after the policy announcement, before trimming some losses.The six-member MPC also continued with the monetary policy stance of withdrawal of accommodation. Four out of six MPC members voted in favour of the rate decision. Announcing the decision, RBI Governor Shaktikanda Das said that inflation broadly has been on a declining trajectory.The RBI had last cut the repo rate by 40 basis points to 4 per cent in May 2020 when the Covid pandemic raged across the country affecting the entire economy, leading to slowdown in demand, production cuts and job losses. Since then, the RBI has hiked the repo rate by 250 points to 6.50 per cent in order to tackle high inflation level after the epidemic subsided.Among the 30 Sensex firms, Power Grid, Infosys, Larsen & Toubro, JSW Steel, UltraTech Cement and Asian Paints were the biggest laggards. Tata Motors, HDFC Bank, Tech Mahindra and ITC were among the gainers during the initial trade."
+text = "."
 
 sentiment_scores = sentiment_analysis(text)
 
